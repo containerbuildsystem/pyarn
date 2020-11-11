8e589710ad004b25b4c441c925ed0e50f0e02533
Athos Ribeiro <athos@redhat.com>
2020-11-11 15:08:01 +0100

    Refactor parser
    Refactor parser by breaking production functions for the same
    non-terminal in multiple functions when actions depend on the right side
    contents of the reduction.

--- pyarn/parser.py
+++ pyarn/parser.py
@@ -17,30 +17,32 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from pyarn.lexer import tokens  # noqa: F401
 
 
+def p_blocks_single(p):
+    """blocks : block"""
+    p[0] = p[1]
+
+
 def p_blocks(p):
-    """blocks : block
-              | blocks block"""
-    if len(p) == 2:
-        p[0] = p[1]
-    else:
-        p[0] = {**p[1], **p[2]}
+    """blocks : blocks block"""
+    p[0] = {**p[1], **p[2]}
 
 
-def p_block(p):
-    """block : title members
-             | pair
-             | comment"""
-    if len(p) == 2:
-        if isinstance(p[1], dict):
-            # is pair
-            p[0] = p[1]
-        else:
-            # TODO: handle comments
-            p[0] = {"pyarn-comment": p[1]}
-    else:
-        # TODO: do we want to separate titles like "foo, bar"?
-        # have in mind they may also be like "foo || bar, biz"
-        p[0] = {p[1]: p[2]}
+def p_block_title(p):
+    """block : title members"""
+    # TODO: do we want to separate titles like "foo, bar"?
+    # have in mind they may also be like "foo || bar, biz"
+    p[0] = {p[1]: p[2]}
+
+
+def p_block_pair(p):
+    """block : pair"""
+    p[0] = p[1]
+
+
+def p_block_comment(p):
+    """block : comment"""
+    # TODO: handle comments
+    p[0] = {"pyarn-comment": p[1]}
 
 
 def p_title(p):
@@ -56,28 +58,28 @@ def p_list(p):
 
 
 def p_members(p):
-    """members : pair
-               | pair INDENT members
-               | title members"""
-    if len(p) == 2:
-        p[0] = p[1]
-    elif isinstance(p[1], str):  # titles
-        p[0] = {p[1]: p[2]}
-    elif isinstance(p[1], dict):  # pairs
-        p[0] = {**p[1], **p[3]}
-    else:
-        raise SyntaxError(
-            f'Error on reducing "members". "{p[1]}" should be str or dict, not {type(p[1])}'
-        )
+    """members : pair"""
+    p[0] = p[1]
+
+
+def p_members_multiple_pairs(p):
+    """members : pair INDENT members"""
+    p[0] = {**p[1], **p[3]}
+
+
+def p_members_nested_title(p):
+    """members : title members"""
+    p[0] = {p[1]: p[2]}
 
 
 def p_pair(p):
-    """pair : STRING STRING NEWLINE
-            | STRING COLON STRING NEWLINE"""
-    if len(p) == 4:
-        p[0] = {p[1]: p[2]}
-    else:
-        p[0] = {p[1]: p[3]}
+    """pair : STRING STRING NEWLINE"""
+    p[0] = {p[1]: p[2]}
+
+
+def p_pair_colon(p):
+    """pair : STRING COLON STRING NEWLINE"""
+    p[0] = {p[1]: p[3]}
 
 
 # TODO: handle indented comments
